fe5cf35740e98184ccad1112ad6ba7d158eb7248
Krettelle NAVARRO <k.navarro@wanadoo.fr>
2020-03-03 14:54:10 +0100

    Gestock version V1.0.0

--- readmeProcess/index.php
+++ readmeProcess/index.php
@@ -1,3 +1,10 @@
 <?php
-    header ('Location: ../index.php');
-    exit();
+    // header ('Location: ../index.php');
+    // exit();
+
+    echo "<pre>";
+    var_dump($_SERVER);
+    echo "</pre>";
+
+    echo $_SERVER['PHP_Self']. "<br>";
+    echo "index dans readmeProcess";
